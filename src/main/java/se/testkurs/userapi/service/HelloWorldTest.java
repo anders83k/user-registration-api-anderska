8dcd5aacc670a0db20ae20c6220f7fc54b375497
anders83k <anders83k@icloud.com>
2026-09-09 10:24:33 +0200

    Add plus test fel

--- src/main/java/se/testkurs/userapi/service/HelloWorldTest.java
+++ src/main/java/se/testkurs/userapi/service/HelloWorldTest.java
@@ -8,6 +8,6 @@ import static org.junit.Assert.assertEquals;
 class HelloWorldTest {
     @Test
     void additionIsCorrect() {
-          assertEquals( 4, 2 + 2);
+          assertEquals( 5, 2 + 2);
     }
 }
