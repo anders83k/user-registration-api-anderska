f31703e15517c8353749105671a7c62abc60f98a
anders83k <anders83k@icloud.com>
2026-09-09 10:22:19 +0200

    Add plus test

---
+++ src/main/java/se/testkurs/userapi/service/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package se.testkurs.userapi.service;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+class HelloWorldTest {
+    @Test
+    void additionIsCorrect() {
+          assertEquals( 4, 2 + 2);
+    }
+}
