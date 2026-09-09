db4fed97570138669ccb8523bf1dc742a0ac50ea
anders83k <anders83k@icloud.com>
2026-09-09 11:08:13 +0200

    Add plus test fel

---
+++ src/test/java/se/testkurs/userapi/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package se.testkurs.userapi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelloWorldTest {
+    @Test
+    void additionIsCorrect() {
+        assertEquals(5, 2 + 2);
+    }
+}
