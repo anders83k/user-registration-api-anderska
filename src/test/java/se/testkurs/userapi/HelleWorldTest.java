5014a54224ac6f9fd2075033e7db043374d8e1a2
anders83k <anders83k@icloud.com>
2026-09-09 10:51:04 +0200

    med maven test

---
+++ src/test/java/se/testkurs/userapi/HelleWorldTest.java
@@ -0,0 +1,13 @@
+
+
+package se.testkurs.userapi.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+class HelloWorldTest {
+    @Test
+    void additionIsCorrect() {
+        Assert.assertEquals(5L, 4L);
+    }
+}
--- src/test/java/se/testkurs/userapi/UserRegistrationApiApplicationTests.java
+++ src/test/java/se/testkurs/userapi/UserRegistrationApiApplicationTests.java
@@ -3,6 +3,8 @@ package se.testkurs.userapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Enda testet som redan finns i basprojektet - kontrollerar bara att
  * Spring-kontexten startar. Alla ovriga tester (enhet, komponent,
@@ -15,5 +17,12 @@ class UserRegistrationApiApplicationTests {
     @Test
     void contextLoads() {
         // Om denna test gar igenom startar hela applikationen korrekt.
+    }
+
+    static class HelloWorldTest {
+        @Test
+        void additionIsCorrect() {
+              assertEquals( 5, 2 + 2);  //ändra tillbaka till 4
+        }
     }
 }
